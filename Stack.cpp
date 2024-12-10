bdbc16aca050ba91817b38f9d631486a4c30c8db
Muhammad Attiq <m.attiq.ce@gmail.com>
2024-12-09 22:39:29 -0800

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,70 +1,114 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
+
 class Node
 {
-private:
-    int data;
-    Node* next;
 public:
-    Node(int d) : data(d) {}
-    friend class STACK;
+    char character;
+    Node* next;
+    Node(char val) : character(val), next(nullptr) {}
 };
-class STACK
+
+class Stack
 {
 private:
-    Node* top;
+    Node* head;
 public:
-    STACK() : top(nullptr) {}
-    void push(int value)
+    Stack() : head(nullptr) {}
+
+    void push(char value)
     {
-        Node* temp = new Node(value);
-        temp->next = top;
-        top = temp;
-        cout << "pushed " << value << " in the stack" << endl;
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
     }
-    void pop()
+
+    char pop()
     {
-        if(Is_empty())
+        if (isEmpty())
         {
-            cout << "stack overflow" << endl;
-            return;
+            return '\0';
         }
-        else
-        {
-            Node* temp = top;
-            int value = top->data;
-            top = top->next;
-            cout << "stack with " << value << " deleted" << endl;
-            value = 0;
-            delete temp;
-        }
+        char value = head->character;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return value;
     }
-    int peek()
+
+    bool isEmpty()
     {
-        if(Is_empty())
+        return head == nullptr;
+    }
+
+    ~Stack()
+    {
+        while (!isEmpty())
         {
-            cout << "stack overflow" << endl;
-            return -1;
-        }
-        else
-        {
-            cout << "top value " << top->data << endl;
+            pop();
         }
     }
-    bool Is_empty()
-    {
-        return top == nullptr;
-    }
-    ~STACK() {}
 };
+
+void reverse_char_array(char* char_array)
+{
+    Stack object;
+    for (int i = 0; char_array[i] != '\0'; i++)
+    {
+        object.push(char_array[i]);
+    }
+    for (int i = 0; char_array[i] != '\0'; i++)
+    {
+        char_array[i] = object.pop();
+    }
+}
+
+bool isBalanced(const char* ptr_array)
+{
+    Stack obj;
+    for (int i = 0; ptr_array[i] != '\0'; i++)
+    {
+        if (ptr_array[i] == '(' || ptr_array[i] == '{' || ptr_array[i] == '[')
+        {
+            obj.push(ptr_array[i]);
+        }
+        else if (ptr_array[i] == ')' || ptr_array[i] == '}' || ptr_array[i] == ']')
+        {
+            if (obj.isEmpty())
+                return false;
+
+            char top = obj.pop();
+            if ((ptr_array[i] == ')' && top != '(') ||
+                (ptr_array[i] == '}' && top != '{') ||
+                (ptr_array[i] == ']' && top != '['))
+            {
+                return false;
+            }
+        }
+    }
+    return obj.isEmpty();
+}
+
 int main()
 {
-    STACK obj;
-    obj.push(1);
-    obj.push(2);
-    obj.pop();
-    obj.pop();
+    char char_array[50];
+    cout << "Enter the string: ";
+    cin.getline(char_array, 50);
+
+    char temp[50];
+    strcpy(temp, char_array);
+    reverse_char_array(temp);
+    cout << "Reversed array: " << temp << endl;
+
+    if (isBalanced(char_array))
+    {
+        cout << "The original string is balanced." << endl;
+    }
+    else
+    {
+        cout << "The original string is not balanced." << endl;
+    }
 
     return 0;
 }
